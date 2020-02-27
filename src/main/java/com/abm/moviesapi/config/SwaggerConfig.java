dcd3e56529fd0eac777b21812aa0740563eaaa76
adam <adam.marek.pietras@gmail.com>
2020-02-28 00:51:54 +0100

    hotfix

--- src/main/java/com/abm/moviesapi/config/SwaggerConfig.java
+++ src/main/java/com/abm/moviesapi/config/SwaggerConfig.java
@@ -1,18 +1,15 @@
-package com.abm.moviesapi;
+package com.abm.moviesapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
-@EnableSwagger2WebMvc
-@Import(BeanValidatorPluginsConfiguration.class)
+@EnableSwagger2
 public class SwaggerConfig {
     @Bean
     public Docket docket(){
@@ -22,6 +19,7 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .build();
     }
+
     //To try out how it look like type in browser
     //localhost:8080/swagger-ui.html
 }
